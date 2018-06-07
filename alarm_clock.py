43368a99d9c862236ca167b99a57fc5d9237f4a6
Madhav Jha <madhavjha2008@gmail.com>
2018-06-07 17:08:27 +0530

    Add files via upload

---
+++ alarm_clock.py
@@ -0,0 +1,28 @@
+from time import sleep
+from datetime import datetime
+import winsound
+
+
+def set_alarm():
+    min = int(input("Enter number of minutes to set the alarm: "))
+
+    t = datetime.now()
+    print("Current time: ", t.strftime('%m/%d/%y %H:%M'))
+
+    sec = min * 60
+
+    if min >= 0:
+        print("Alarm will ring after " + str(min) + ' minute(s)')
+        sleep(sec)
+        print("Get up!")
+        winsound.Beep(1500, 5000)
+
+    else:
+        print("Please enter valid value!")
+
+
+if __name__ == "__main__":
+    set_alarm()
+
+
+
